722a2154bdeabfc0fb21e357c2028a1edf9b1e4c
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-08-23 08:59:52 +0000

    [Start]3-6

---
+++ lesson6-2.rb
@@ -0,0 +1,20 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+count = gets.to_i
+c = 1
+while c <= count do
+
+  puts "#{c} 回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a+b= #{a+b}"
+  puts "a-b= #{a-b}"
+  puts "a*b= #{a*b}"
+  puts "a/b= #{a/b}"
+  c += 1
+  if c == count + 1
+    puts "計算を終了します"
+  break
+  end
+end
