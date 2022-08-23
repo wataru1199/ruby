20e4e16680da24b8345a9443a248a0ed7177c9dc
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-08-23 09:03:37 +0000

    [Fix]3-6

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -4,10 +4,11 @@ count = gets.to_i
 c = 1
 while c <= count do
 
-  puts "#{c} 回目の計算"
+  puts "#{c}回目の計算"
   puts "2つの値を入力してください"
   a = gets.to_i
   b = gets.to_i
+  puts "計算結果を表示します"
   puts "a+b= #{a+b}"
   puts "a-b= #{a-b}"
   puts "a*b= #{a*b}"
